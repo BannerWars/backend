435e16cc4394926bb5d9dcd28d32c63cf9165862
Drake Vorndran <drakevorndranoffical@gmail.com>
2020-06-04 14:24:06 -0400

    Set up server, implemented auth

---
+++ server.js
@@ -0,0 +1,30 @@
+// imports
+const dotenv = require('dotenv').config()
+const express = require("express")
+const http = require("http")
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const config = require('./config')
+const mongoose = require('mongoose')
+
+
+// controllers
+const index = require("./controllers/index")
+const AuthController = require("./controllers/AuthController")
+
+// middleware
+const app = express()
+const port = process.env.PORT || 4000
+
+// connection
+mongoose.connect(process.env.mongoUri)
+app.use(cors())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+
+app.use(index)
+app.use('/users', AuthController)
+
+// server
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Listening on port ${port}`));
